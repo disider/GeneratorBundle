45827b78b4fa864af1893104d24350ff0e663558
fedemame <fede.mame@gmail.com>
2015-08-05 11:37:57 +0200

    Implemented api controller generator

---
+++ Command/GenerateApiControllerCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Diside\GeneratorBundle\Command;
+
+
+use Diside\GeneratorBundle\Generator\ApiControllerGenerator;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class GenerateApiControllerCommand extends BaseGenerateDoctrineCommand
+{
+
+    /** @var bool */
+    private $security;
+
+    protected function configure()
+    {
+        $this
+            ->setName('diside:generate:api-controller')
+            ->addArgument('entity', InputArgument::REQUIRED, 'A entity name')
+            ->addOption('add-security', null, InputOption::VALUE_NONE,  'Add security annotation');
+    }
+
+    protected function preExecute(InputInterface $input, OutputInterface $output)
+    {
+        $this->security = $input->getOption('add-security');
+    }
+
+    protected function createGenerator()
+    {
+        $controller = new ApiControllerGenerator($this->getContainer()->get('filesystem'), $this->getContainer()->get('templating'));
+        $controller->setSecurity($this->security);
+
+        return $controller;
+    }
+
+    protected function writeOutput(OutputInterface $output, $outputMessage)
+    {
+        $output->writeln(array(
+            'The new controller file has been created.',
+            '',
+            'Add these lines to your services:',
+            '',
+            $outputMessage,
+        ));
+    }
+
+}
---
+++ Generator/ApiControllerGenerator.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Diside\GeneratorBundle\Generator;
+
+use Doctrine\ORM\Mapping\ClassMetadataInfo;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+
+
+class ApiControllerGenerator extends BaseGenerator
+{
+    /** @var bool */
+    private $security;
+
+    public function setSecurity($value)
+    {
+        $this->security = $value;
+    }
+
+    public function generate(BundleInterface $bundle, $entity, ClassMetadataInfo $metadata)
+    {
+        $className = 'Api' . $entity . 'Controller';
+        $classPath = $bundle->getPath() . '/Controller/' . $className . '.php';
+
+        if (file_exists($classPath)) {
+            throw new \RuntimeException(sprintf('Unable to generate the %s class as it already exists under the %s file', $className, $classPath));
+        }
+
+        $this->generateBaseController($bundle);
+        $this->generateException($bundle);
+        $this->generateModel($bundle);
+        $this->generateListener($bundle);
+        $this->generateEntityController($bundle, $entity, $classPath);
+
+        return $this->render('DisideGeneratorBundle:Controller:apiConfig.html.twig', array());
+
+    }
+
+    protected function generateBaseController(BundleInterface $bundle)
+    {
+        $baseControllerPath = $bundle->getPath() . '/Controller/BaseApiController.php';
+
+        if (!$this->filesystem->exists($baseControllerPath)){
+            $this->renderFile('DisideGeneratorBundle:Controller:baseApiController.php.twig', $baseControllerPath, array(
+                'namespace' => $bundle->getNamespace(),
+            ));
+        }
+    }
+
+    protected function generateException(BundleInterface $bundle)
+    {
+        $exceptionPath = $bundle->getPath() . '/Exception/ApiException.php';
+
+        if (!$this->filesystem->exists($exceptionPath)){
+            $this->renderFile('DisideGeneratorBundle:Exception:apiException.php.twig', $exceptionPath, array(
+                'namespace' => $bundle->getNamespace(),
+            ));
+        }
+    }
+
+    protected function generateModel(BundleInterface $bundle)
+    {
+        $exceptionPath = $bundle->getPath() . '/Model/ApiProblem.php';
+
+        if (!$this->filesystem->exists($exceptionPath)){
+            $this->renderFile('DisideGeneratorBundle:Model:apiProblem.php.twig', $exceptionPath, array(
+                'namespace' => $bundle->getNamespace(),
+            ));
+        }
+    }
+
+    protected function generateListener(BundleInterface $bundle)
+    {
+        $exceptionPath = $bundle->getPath() . '/Listener/ApiExceptionListener.php';
+
+        if (!$this->filesystem->exists($exceptionPath)){
+            $this->renderFile('DisideGeneratorBundle:Listener:apiExceptionListener.php.twig', $exceptionPath, array(
+                'namespace' => $bundle->getNamespace(),
+            ));
+        }
+    }
+
+    protected function generateEntityController(BundleInterface $bundle, $entity, $classPath)
+    {
+        $this->renderFile('DisideGeneratorBundle:Controller:apiEntityController.php.twig', $classPath, array(
+            'security' => $this->security,
+            'namespace' => $bundle->getNamespace(),
+            'entity' => $entity,
+            'path' => $this->getPath($entity),
+            'route_prefix' => $this->getEntityRoutePrefix($entity),
+        ));
+    }
+}
